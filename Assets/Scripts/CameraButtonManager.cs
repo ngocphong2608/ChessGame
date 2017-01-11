6eaa40f57f98510e14dbbbe7a26e0d10eecf771f
ngocphong2608 <ttphong2608@gmail.com>
2017-01-11 17:13:19 +0700

    [Update] Moving the camera

---
+++ Assets/Scripts/CameraButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraButtonManager : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GameObject.Find("Main Camera").GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.Log("Can't find camera animator.");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // side: left, right, white, black
+    public void MoveCamera(string side)
+    {
+        if (side == "left")
+        {
+            animator.SetTrigger("LeftMotion");
+        } else if (side == "right")
+        {
+            animator.SetTrigger("RightMotion");
+        } else if (side == "white")
+        {
+            animator.SetTrigger("WhiteTurn");
+        } else if (side == "black")
+        {
+            animator.SetTrigger("BlackTurn");
+        }
+    }
+}
